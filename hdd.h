0eddb3e333fde9ededd1f3b797df616af637dff1
Ozanis <37501284+Ozanis@users.noreply.github.com>
2018-11-12 00:18:57 +0200

    Update hdd.h

--- hdd.h
+++ hdd.h
@@ -1,22 +1,27 @@
+#ifndef UNTITLED4_HDD_H
+#define UNTITLED4_HDD_H
+
 #include <sys/statvfs.h>
 #include <iostream>
 #include <stdio.h>
 
-int hdd_1() {
+double hdd_1() {
     struct statvfs buf;
-
-    if (statvfs(".", &buf) == -1)
-        return -1;
+    if (statvfs(".", &buf) == -1) return -1;
     else {
-    int hdd_1=(double)buf.f_bavail * buf.f_frsize/1000000000;
-    return hdd_1;
-    
- int hdd_2() {
-    struct statvfs buf;
-    if (statvfs(".", &buf) == -1)
-        return -1;
-    int hdd_2=(double)buf.f_blocks * buf.f_frsize)/1000000000-hdd_1();
+        double hdd = (double) buf.f_bavail * buf.f_frsize / 1000000000;
+        return hdd;
     }
-    
-    return hdd_2;
 }
+
+double hdd_2() {
+    struct statvfs buf;
+    if (statvfs(".", &buf) == -1) return -1;
+    else{
+        double hdd=(double)buf.f_blocks * buf.f_frsize/1000000000-hdd_1();
+        return hdd;
+    }
+}
+
+
+#endif //UNTITLED4_HDD_H
