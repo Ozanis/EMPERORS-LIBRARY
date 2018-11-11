0d02c90bb2f9958ccd2da60ce24404fc31c46062
Ozanis <37501284+Ozanis@users.noreply.github.com>
2018-11-12 00:18:35 +0200

    Update server.h

--- server.h
+++ server.h
@@ -2,21 +2,54 @@
 #define UNTITLED4_SERVER_H
 
 #include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <string>
-
-int sock;
-struct sockaddr_in addr;
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
 using namespace std;
 
-int server(int cp, int _r1, int _r2, int hdd_1, int hdd_2) {
-    int BUFFER_MAX_SIZE = 25;
+#define MAXPACKETSIZE 4096
+
+class Server
+{
+public:
+    int sockfd, newsockfd, n, pid;
+    struct sockaddr_in serverAddress;
+    struct sockaddr_in clientAddress;
+    char msg[ MAXPACKETSIZE ];
+    static string Message;
+    void setup(int port);
+    double Send(double cp, double _r1, double _r2, double hdd_1, double hdd_2);
+    void clean();
+
+private:
+
+};
+
+void Server::setup(int port)
+{
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    memset(&serverAddress,0,sizeof(serverAddress));
+    serverAddress.sin_family=AF_INET;
+    serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+    serverAddress.sin_port=htons(port);
+    bind(sockfd,(struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    listen(sockfd, 1);
+}
+
+
+double Server::Send(double cp, double _r1, double _r2, double hdd_1, double hdd_2)
+{
     string  message=to_string(cp)+"/"+to_string(_r1)+"/"+to_string(_r2)+"/"+to_string(hdd_1)+"/"+to_string(hdd_2);
-    if (message.size()>=BUFFER_MAX_SIZE) return 1;
+    if (message.size()>=MAXPACKETSIZE) return 1;
     int i;
     char *msg = (char*)malloc(sizeof(char));
     for(i=1; i<=message.size(); i++){
@@ -24,31 +57,19 @@ int server(int cp, int _r1, int _r2, int hdd_1, int hdd_2) {
         if (i<message.size()) msg=(char*)realloc(msg, (i+1)*sizeof(char));
         if(i==message.size()) break;
     }
-
-    char buf[sizeof(msg)];
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock < 0)
-    {
-        perror("socket");
-        exit(1);
-    }
-
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(4430);
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        perror("connect");
-        exit(2);
-    }
-
-    send(sock, msg, sizeof(msg), 0);
-    //recv(sock, buf, sizeof(msg), 0);
-
-   // printf(buf);
-    close(sock);
-
+    send(newsockfd,&msg, sizeof(msg),0);
     return 0;
+}
+
+
+void Server::clean()
+{
+    Message = "";
+    memset(msg, 0, MAXPACKETSIZE);
+    close(sockfd);
+    close(newsockfd);
+    delete(&sockfd);
+    delete(&newsockfd);
 }
 
 #endif //UNTITLED4_SERVER_H
