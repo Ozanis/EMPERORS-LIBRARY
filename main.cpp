35fe0fa48dd212ba95f9e9cac3a9cd9c1ac02b17
Ozanis <maximolodets@gmail.com>
2018-12-05 17:04:20 +0200

    Commit

--- main.cpp
+++ main.cpp
@@ -13,24 +13,25 @@ int main() {
     long _ncp = sysconf(_SC_NPROCESSORS_ONLN);
     string data;
     Server cnct;
-    do {
-        vector<CPUData> entries1;
-        vector<CPUData> entries2;
+    if (!cnct.set_up()) return 1;
+    else{
+        do {
+            vector<CPUData> entries1;
+            vector<CPUData> entries2;
 
-        ReadStatsCPU(entries1);
-        this_thread::sleep_for(chrono::milliseconds(200));
-        ReadStatsCPU(entries2);
-        PrintStats(entries1, entries2, _cp);
+            ReadStatsCPU(entries1);
+            this_thread::sleep_for(chrono::milliseconds(200));
+            ReadStatsCPU(entries2);
+            PrintStats(entries1, entries2, _cp);
 
-        process_mem_usage(_r1, _r2);
-        physical_mem_usage(_hdd1, _hdd2);
-        data=to_string(_cp)+"/"+to_string(_ncp)+"/"+to_string(_r1)+"/"+to_string(_r2)+"/"+to_string(_hdd1)+"/"+to_string(_hdd2);
-        cout << data << endl;
-        cnct.Send(data);
-        sleep(4);
-    } while (cnct.Send(data));
-
-    return 0;
+            process_mem_usage(_r1, _r2);
+            physical_mem_usage(_hdd1, _hdd2);
+            data=to_string(_cp)+"/"+to_string(_ncp)+"/"+to_string(_r1)+"/"+to_string(_r2)+"/"+to_string(_hdd1)+"/"+to_string(_hdd2);
+            cout << data << endl;
+            cnct.Send(data);
+            sleep(4);
+        } while (cnct.Send(data));
+    } return 0;
 }
 
 #elif _WIN32
--- server.h
+++ server.h
@@ -10,7 +10,7 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-
+using std::cerr;
 using std::string;
 using std::to_string;
 
@@ -19,37 +19,57 @@ class Server
 private:
     uint16_t port = 4430;
     struct sockaddr_in serverAddress;
-    //struct sockaddr_in clientAddress;
+    struct sockaddr_in clientAddress;
 
 public:
     int sockfd, newsockfd;
     char * message;
     Server();
     ~Server();
+    bool set_up();
     bool Send(string data);
+    int len;
 };
 
 Server::Server(){
-    memset(message, 0, sizeof(*message));
-    close(sockfd);
-    close(newsockfd);
+    this->sockfd=socket(AF_INET,SOCK_STREAM,0);
+    if(!this->sockfd) cerr << "Error of socket opening";
+    this->serverAddress.sin_family=AF_INET;
+    this->serverAddress.sin_addr.s_addr=htonl(INADDR_LOOPBACK);
+    this->serverAddress.sin_port=htons(this->port);
+
+    this->newsockfd=socket(AF_INET, SOCK_STREAM, 0);
+    this->clientAddress.sin_family=AF_INET;
+    this->clientAddress.sin_addr.s_addr=htonl(INADDR_LOOPBACK);
+    this->clientAddress.sin_port=htons(this->port);
+
+    this->len = sizeof(clientAddress);
 }
 
 Server::~Server(){
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    memset(message, 0, sizeof(*message));
     memset(&serverAddress,0,sizeof(serverAddress));
-    serverAddress.sin_family=AF_INET;
-    serverAddress.sin_addr.s_addr=htonl(INADDR_LOOPBACK);
-    serverAddress.sin_port=htons(port);
-    bind(sockfd,(struct sockaddr *)&serverAddress, sizeof(serverAddress));
-    listen(sockfd, 1);
+    close(sockfd);
 }
 
+bool Server::set_up(){
+    if((bind(sockfd,(struct sockaddr *)&serverAddress, sizeof(serverAddress)))<0) {
+        cerr << "Error of binding";
+        return false;
+    } listen(sockfd, 1);
+
+    if(accept(sockfd,(sockaddr*)&clientAddress, (socklen_t*)&len)<0) {
+        cerr << "Acception error";
+        return false;
+    };
+
+    return true;
+}
 
 bool Server::Send(string data){
     message = new char[data.length()];
     for (int i = 0; i <= data.length(); i++) {
       message[i] = data[i];
     }
-    return send(newsockfd, message, sizeof(message), 0) < 0;
+    return send(sockfd, message, sizeof(message), 0) < 0;
 }
